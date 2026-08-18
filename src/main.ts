d7dcc834d878cbf27afe5638ce402e66fa52ba4d
Maxwell Gachuhi <gachuhimaxwell1@gmail.com>
2026-08-18 14:33:26 +0300

    feat(backend): configure swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('BMS API')
+    .setDescription('Business Management System API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/v1/docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
